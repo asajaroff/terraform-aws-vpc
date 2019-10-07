ed0dabbd6a603edd63df1122aa1bc07ab0f964fe
Alejandro Sajaroff <asajaroff@gmail.com>
2019-10-07 15:52:18 -0300

    Local render of tags variable for better naming

--- main.tf
+++ main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   version = "~> 2.0"
   region  = "us-east-1"
 }
+locals {
+ rendered_tags = {
+    "Project"     = "${var.tags["Project"]}"
+    "Namespace"   = "${var.tags["Namespace"]}"
+    "Environment" = "${var.tags["Environment"]}"
+    "Stage"       = "${var.tags["Stage"]}"
+    "Team"        = "${var.tags["Team"]}"
+    "Owner"       = "${var.tags["Owner"]}"  
+  }
+}
 
 resource "aws_vpc" "main" {
   cidr_block                       = var.cidr_block
@@ -11,19 +21,26 @@ resource "aws_vpc" "main" {
   enable_classiclink               = var.enable_classiclink
   enable_classiclink_dns_support   = var.enable_classiclink_dns_support
   assign_generated_ipv6_cidr_block = var.assign_ipv6_block
-  tags                             = var.tags
+  tags = merge( {
+    "Name"  = "${var.tags["Project"]}-${var.tags["Environment"]}-${var.tags["Stage"]}-VPC" ,
+    "Description" = "${var.tags["Project"]} VPC"}, local.rendered_tags 
+    )
 }
 
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "Default Security Group"
-  }
+   tags = merge( {
+    "Name"  = "${var.tags["Project"]}-${var.tags["Environment"]}-${var.tags["Stage"]}-VPC-SecurityGroup" ,
+    "Description" = "${var.tags["Project"]} VPC: Default Security Group"}, 
+    local.rendered_tags)
 }
 
 resource "aws_internet_gateway" "default" {
   vpc_id = aws_vpc.main.id
-  tags   = var.tags
+   tags = merge( {
+    "Name"  = "${var.tags["Project"]}-${var.tags["Environment"]}-${var.tags["Stage"]}-InternetGategay" ,
+    "Description" = "${var.tags["Project"]} VPC default Internet Gateway"}, 
+    local.rendered_tags)
 }
 
 // condition ? true_val : false_val
